3675147bd6a4a01406d042df3c7f50bb165cd567
DoMinhTri <dominhtri@ymail.com>
2021-08-05 06:44:29 +0700

    Update Server.js

--- Server.js
+++ Server.js
@@ -20,8 +20,7 @@ io.on("connection", socket => {
 	});
 });
 //////////////////////////////////////////////////////////////
-app.get("/", function(req, res){
-	res.render("index");
+app.get("/", function(req, res){res.render("index");
 }); 
 
 
